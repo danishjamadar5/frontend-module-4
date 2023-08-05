9b08ea56b8ab7233df04b000d032a51da75d3886
danish <danishjamadar9089@gmail.com>
2023-08-05 23:34:12 +0530

    file add

--- src/Redux/Actions/CardAction.js
+++ src/Redux/Actions/CardAction.js
@@ -15,30 +15,3 @@ export const fetchPostsRequest = () => ({
     type: types.FETCH_POSTS_FAILURE,
     payload: error,
   });
-
-
-//   export const fetchPosts = () => async (dispatch) => {
-//     dispatch(fetchPostsRequest());
-  
-//     try {
-//       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-//       const data = await response.json();
-  
-//       dispatch(fetchPostsSuccess(data));
-//     } catch (error) {
-//       dispatch(fetchPostsFailure(error.message));
-//     }
-//   };
-// export const fetchData = ()=>{
-//     return (dispatch)=>{
-//         dispatch(fetchDataRequest())
-//         fetch("https://jsonplaceholder.typicode.com/posts")
-//         .then(async response =>{
-//             dispatch(fetchDataSuccess(await response.json()))
-//         } )
-        
-//         .catch(error=>dispatch(fetchDataFailue(error)))
-//     }
-// }
-
-// 
